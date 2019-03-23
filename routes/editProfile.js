d127c78f2abc00da1773e06c093a9c1e673df3a0
juyoungkim304 <juyoung.kim304@gmail.com>
2019-03-23 15:58:12 -0500

    More additions to edit profiles

--- routes/editProfile.js
+++ routes/editProfile.js
@@ -9,5 +9,21 @@ module.exports = {
             console.log("Reached editProfile.js");
             res.render('pages/editProfile.ejs',{pcbg: result, index: req.params[0]});
         });
+    },
+
+    editedPage: (req, res) => {
+        //let index = req.params[0];
+        //let first_name = req.body.first_name;
+        //let last_name = req.body.last_name;
+
+        //let query = "UPDATE
+
+        db.query(query, (err, result) => {
+            if (err){
+                res.redirect('/');
+            }
+            console.log("Edited editProfile.js");
+            //res.redirect('/^\/profile\$(\d+)/');
+        });
     }
 }
