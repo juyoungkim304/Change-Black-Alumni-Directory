c1d7f3c68c0d4450832abf93a9ec916d89706ed9
stella <tingyu.wang@vanderbilt.edu>
2019-12-28 14:57:58 -0600

    addProfile

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ global.db = db;
 const {profilePage} = require('./routes/profile.js');
 const {searchPage} = require('./routes/searchresults.js');
 const {editPage, editedPage} = require('./routes/editProfile.js');
+const {addProfilePage} = require('./routes/addProfile.js');
+
 
 app.set('view engine', 'ejs');
 
@@ -40,6 +42,8 @@ app.get(/^\/searchresults=([0-9]+)&(.*)/, searchPage);
 app.get(/^\/editprofile\$(\d+)/, editPage);
 
 app.post(/^\/editprofile\$(\d+)/, editedPage);
+
+app.get(/^\/addProfile\$(\d+)/, addProfilePage);
 
 app.listen('8080');
 console.log("Listening at 8080...");
