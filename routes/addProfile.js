b2ea14d2b56af5b0fa11e6a4c8dd7566a37223fb
Bao Pham <bao.d.pham@vanderbilt.edu>
2020-01-26 16:49:17 -0600

    Fixed link to Add Profile

---
+++ routes/addProfile.js
@@ -0,0 +1,39 @@
+module.exports = {
+    addProfile: (req, res) => {
+        res.render('pages/addProfile.ejs');
+
+    },
+
+    addedProfile: (req, res) => {
+
+        let first_name = req.body.first_name;
+        let last_name = req.body.last_name;
+        let preferred_name = req.body.preferred_name;
+        let occupation = req.body.occupation;
+        let relation = req.body.relation;
+        let major_or_program = req.body.major_or_program;
+        let location = req.body.location;
+        let phone = req.body.phone;
+        let email = req.body.email;
+        let pic = ""
+        let external_link = ""
+
+        var fields = [first_name, last_name, preferred_name, occupation, relation, major_or_program, location, phone, email, pic, external_link]
+
+        let query = "INSERT INTO pcbg (first_name, last_name, preferred_name, occupation, relation, major_or_program, location, phone, email, pic, external_link) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        db.query(query, fields, (error) => {
+
+            if (error){
+                res.redirect('/');
+                console.log(error);
+            }
+            console.log("Added profile");
+            res.redirect('/');
+        });
+
+        //res.render('pages/addProfile.ejs');
+
+    //});
+    }
+}
