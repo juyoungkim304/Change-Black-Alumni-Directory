bebdaa9a4d62dac2cd56281156524bb776a3495a
Tingyu Wang <tingyu.wang@vanderbilt.edu>
2020-02-02 16:17:54 -0600

    delete method

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+// add bodyParser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// var mongodb = require('mongodb').mongodb;
+// global.mongodb = mongodb;
+
+// const db = require('./config/db');
 
 var db = mysql.createConnection({
     host: 'blackalumnidirectory.cxs1kr4seklv.us-east-1.rds.amazonaws.com',
@@ -21,8 +29,9 @@ global.db = db;
 
 const {profilePage} = require('./routes/profile.js');
 const {searchPage} = require('./routes/searchresults.js');
-const {editPage, editedPage} = require('./routes/editProfile.js');
-const {addProfile, addedProfile} = require('./routes/addProfile.js');
+// const {editPage, editedPage} = require('./routes/editProfile.js');
+// const {addProfile, addedProfile} = require('./routes/addProfile.js');
+const {deletePage} = require('./routes/deleteProfile.js');
 
 
 app.set('view engine', 'ejs');
@@ -39,15 +48,19 @@ app.get(/^\/profile\$(\d+)/, profilePage);
 
 app.get(/^\/searchresults=([0-9]+)&(.*)/, searchPage);
 
-app.get(/^\/editprofile\$(\d+)/, editPage);
+// app.get(/^\/editprofile\$(\d+)/, editPage);
 
-app.post(/^\/editprofile\$(\d+)/, editedPage);
+// app.post(/^\/editprofile\$(\d+)/, editedPage);
 
-// app.get(/^\/addProfile\$(\d+)/, addProfilePage);
+// var deletePage = express.Router().delete;
 
-app.get(/^\/addprofile/, addProfile);
+app.get(/^\/deleteprofile\$(\d+)/, deletePage);
 
-app.post(/^\/addprofile/, addedProfile);
+// app.post(/^\/deleteprofile\$(\d+)/, deletedPage);
+
+// app.get(/^\/addprofile/, addProfile);
+
+// app.post(/^\/addprofile/, addedProfile);
 
 app.listen('8080');
 console.log("Listening at 8080...");
