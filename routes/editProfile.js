4e449eb36f3d389576def29f7ee56aed068e5f02
tabithasylee <tabitha.see.ya.lee@vanderbilt.edu>
2020-02-09 17:39:59 -0600

    Added file upload functionality

--- routes/editProfile.js
+++ routes/editProfile.js
@@ -1,41 +1,46 @@
 module.exports = {
-    editPage: (req, res) => {
-        let query = "SELECT * FROM pcbg";
+  editPage: (req, res) => {
+    let query = "SELECT * FROM pcbg";
 
-        db.query(query, (err, result) => {
-            if (err){
-                res.redirect('/');
-            }
-            console.log("Reached editProfile.js");
-            res.render('pages/editProfile.ejs',{pcbg: result, index: req.params[0]});
-        });
-    },
+    db.query(query, (err, result) => {
+      if (err) {
+        res.redirect('/');
+      }
+      console.log("Reached editProfile.js");
+      res.render('pages/editProfile.ejs', {
+        pcbg: result,
+        index: req.params[0]
+      });
+    });
+  },
 
-    editedPage: (req, res) => {
-        //REQUIRES body-parser npm plugin
-        let index = parseInt(req.params[0], 10) + 1;
-        let first_name = req.body.first_name;
-        let last_name = req.body.last_name;
-        let preferred_name = req.body.preferred_name;
-        let bio = req.body.bio;
-        let relation = req.body.relation;
-        let major_or_program = req.body.major_or_program;
-        let location = req.body.location;
-        let phone = req.body.phone;
-        let email = req.body.email;
+  editedPage: (req, res) => {
+    //REQUIRES body-parser npm plugin
+    let index = parseInt(req.params[0], 10) + 1;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let preferred_name = req.body.preferred_name;
+    let bio = req.body.bio;
+    let relation = req.body.relation;
+    let major_or_program = req.body.major_or_program;
+    let location = req.body.location;
+    let phone = req.body.phone;
+    let email = req.body.email;
+    let pic = 'http://changedirectory.s3.amazonaws.com/' + req.body.file_name;
 
-        var fields = [first_name, last_name, preferred_name, relation, major_or_program, location, phone, email, index]
+    var fields = [first_name, last_name, preferred_name, relation, major_or_program, location, phone, email, pic, index];
 
-        let query = "UPDATE pcbg SET first_name = ?, last_name = ?, preferred_name = ?, relation = ?, major_or_program = ?, location = ?, phone = ?, email = ? WHERE uid = ?";
+    let query = "UPDATE pcbg SET first_name = ?, last_name = ?, preferred_name = ?, relation = ?, major_or_program = ?, location = ?, phone = ?, email = ?, pic = ? WHERE uid = ?";
 
-        db.query(query, fields, (err) => {
-            if (err){
-              console.log(err);
-                res.redirect('/');
-                return;
-            }
-            console.log("Updated profile with uid " + index);
-            res.redirect('/profile$' + req.params[0]);
-        });
-    }
+    db.query(query, fields, (err) => {
+      if (err) {
+        console.log(err);
+        res.redirect('/');
+        return;
+      }
+      console.log(fields);
+      console.log("Updated profile with uid " + index);
+      res.redirect('/profile$' + req.params[0]);
+    });
+  }
 }
