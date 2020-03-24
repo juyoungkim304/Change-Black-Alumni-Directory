f1bd08c72274a9057656b5bb31b8fb99fdb0916e
Juyoung Kim <juyoung.kim304@gmail.com>
2020-03-24 18:02:18 -0400

    Fixed indexing on profiles

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,16 @@
 module.exports = {
 	profilePage: (req, res) => {
-		let query = "SELECT * FROM pcbg";
+		let query = "SELECT * FROM pcbg WHERE uid = ?";
+		const uid = parseInt(req.params[0]) + 1;
+		console.log(uid);
 
-		db.query(query, (err, result) => {
+		db.query(query, uid, (err, result) => {
 			if (err){
 				res.redirect('/');
 			}
 			console.log("Reached profile.js");
-			res.render('pages/profile.ejs',{pcbg: result, index: req.params[0]});
+			console.log(result);
+			res.render('pages/profile.ejs',{pcbg: result});
 		});
 		
 
