317644e7dc1471dec014d5fd9cb232485e95ed85
Juyoung Kim <juyoung.kim304@gmail.com>
2020-03-24 18:03:46 -0400

    Revert "Fixed indexing on profiles"
    This reverts commit f1bd08c72274a9057656b5bb31b8fb99fdb0916e.

--- routes/profile.js
+++ routes/profile.js
@@ -1,16 +1,13 @@
 module.exports = {
 	profilePage: (req, res) => {
-		let query = "SELECT * FROM pcbg WHERE uid = ?";
-		const uid = parseInt(req.params[0]) + 1;
-		console.log(uid);
+		let query = "SELECT * FROM pcbg";
 
-		db.query(query, uid, (err, result) => {
+		db.query(query, (err, result) => {
 			if (err){
 				res.redirect('/');
 			}
 			console.log("Reached profile.js");
-			console.log(result);
-			res.render('pages/profile.ejs',{pcbg: result});
+			res.render('pages/profile.ejs',{pcbg: result, index: req.params[0]});
 		});
 		
 
