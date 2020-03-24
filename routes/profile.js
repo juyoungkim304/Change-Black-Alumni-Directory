70e16d9cbeb93b0f88bb0f8f5771617966efedfb
Juyoung Kim <juyoung.kim304@gmail.com>
2020-03-24 18:09:14 -0400

    Actual index fix for profiles

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,14 @@
 module.exports = {
 	profilePage: (req, res) => {
-		let query = "SELECT * FROM pcbg";
+		let query = "SELECT * FROM pcbg WHERE uid = ?";
+		const uid = parseInt(req.params[0]) + 1;
 
-		db.query(query, (err, result) => {
+		db.query(query, uid,(err, result) => {
 			if (err){
 				res.redirect('/');
 			}
 			console.log("Reached profile.js");
-			res.render('pages/profile.ejs',{pcbg: result, index: req.params[0]});
+			res.render('pages/profile.ejs',{pcbg: result, index: 0});
 		});
 		
 
