ab376960ce4ee4d6fc0a24251e013d36f7a36a3b
Juyoung Kim <juyoung.kim304@gmail.com>
2020-06-28 17:36:16 -0400

    Revert login changes

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var strategy = new Auth0Strategy(
         clientID: process.env.AUTH0_CLIENT_ID,
         clientSecret: process.env.AUTH0_CLIENT_SECRET,
         callbackURL:
-            'https://immense-harbor-88562.herokuapp.com/login/callback'
+            'https://immense-harbor-88562.herokuapp.com/callback'
     },
     function (accessToken, refreshToken, extraParams, profile, done) {
         // accessToken is the token to call Auth0 API (not needed in the most cases)
--- routes/auth.js
+++ routes/auth.js
@@ -18,7 +18,7 @@ router.get('/login', passport.authenticate('auth0', {
 });
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
-router.get('/login/callback', function (req, res, next) {
+router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
     if (err) { return next(err); }
     if (!user) { return res.redirect('/login'); }
