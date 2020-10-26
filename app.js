06e3a53aeb7337981770238d0c05d86a18e50c91
Juyoung Kim <juyoung.kim304@gmail.com>
2020-10-26 19:13:22 -0400

    Callback change

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var strategy = new Auth0Strategy(
         clientID: process.env.AUTH0_CLIENT_ID,
         clientSecret: process.env.AUTH0_CLIENT_SECRET,
         callbackURL:
-            'http://localhost:5000/callback'
+            'https://immense-harbor-88562.herokuapp.com'
     },
     function (accessToken, refreshToken, extraParams, profile, done) {
         // accessToken is the token to call Auth0 API (not needed in the most cases)
